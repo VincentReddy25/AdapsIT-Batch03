e6cf7e1aef41701041cf676624c8d510a0a5bc18
vincent reddy <vincentreddy25@gmail.com>
2025-12-23 11:09:18 +0530

    ...

---
+++ JavaScript/events.js
@@ -0,0 +1,17 @@
+
+
+
+
+function changeMe() {
+
+    let display = document.getElementById("display")
+    display.innerHTML = "bye"
+
+    let box = document.getElementById("box")
+    box.style.color = "red"
+
+    box.classList.toggle("new-box")
+    
+}
+
+
